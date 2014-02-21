51cc165d5bb88050ed574dc323d53086cdb716d6
shrimpboyho <shrimpboyhoftw@gmail.com>
2014-02-21 13:12:49 -0500

    Got a base server side flow setup

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+
+// Configure static
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+  
+});
+
+// Initially server index
+app.get('/',function(request,response){       
+  response.sendfile(__dirname+ "/public/index.html");
+});
+
+// Handle new blog post request
+app.get('/sendtoken:token',function(request,response){ 
+	var access_token = request.params.token;
+	response.end("Got token: " + access_token);
+});
+
+// Catch 404s
+app.get('*', function(req, res){
+  res.sendfile(__dirname+ "/public/404.html");
+});
+
+// Listen
+app.listen(process.env.PORT || 5000);
+console.log('Listening on port 5000');
